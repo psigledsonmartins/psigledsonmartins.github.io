4452288a54d7c2fa9c2b255ea6e59f4a81127d3f
Gabriel Areco <areco23@gmail.com>
2024-12-02 14:06:37 -0400

    BaseURL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://garecot.github.io",
+  // site: "https://garecot.github.io",
   // base: "",
 });
