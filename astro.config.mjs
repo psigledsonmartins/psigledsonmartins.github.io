cf3eeb4eee026b9d7555171e05dc66961fe1ec3e
Gabriel Areco <areco23@gmail.com>
2024-12-02 16:48:42 -0400

    Mudanças

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://garecot.github.io",
   // base: "",
+  site: "https://garecot.github.io",
+
+  integrations: [tailwind(), icon()],
 });
