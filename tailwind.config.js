3d20a7a20f7f24268c93f56f02cbe5e6e1928f23
Gabriel Areco <areco23@gmail.com>
2024-12-05 16:31:35 -0400

    FEAT: Mudança de fonte

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ import daisyui from "daisyui";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        caveat: ["Caveat", "cursive"],
+      }
+    },
   },
   plugins: [daisyui],
   daisyui: {
