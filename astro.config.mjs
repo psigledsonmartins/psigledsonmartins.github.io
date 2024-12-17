9f2dd136e473dc9e82b7674ee05670a176515a3f
Gabriel Areco <areco23@gmail.com>
2024-12-17 13:42:44 -0400

    FIX: URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   // base: "",
-  site: "psigledsonmartins.github.io",
+  site: "https://psigledsonmartins.github.io",
 
   integrations: [tailwind(), icon()],
 });
